f246f87a4cce3d31235d7b6f6d37ef3a0304d148
Aymen kamhan <aymenqamhan2003@gmail.com>
2025-07-26 11:21:38 -0700

    rename folder Aymen to name login

--- labs/lab2/Django_labs/lab1/login/views.py
+++ labs/lab2/Django_labs/lab1/login/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("صفحة ayemn شغالة ")
+    return HttpResponse("صفحة login شغالة ")
